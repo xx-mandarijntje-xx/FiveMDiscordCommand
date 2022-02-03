aaff502072c9a893800d9e5d290904f129ac46ba
xx-mandarijntje-xx <98543952+xx-mandarijntje-xx@users.noreply.github.com>
2022-02-03 03:15:04 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'RL'
+description '/discord command for FiveM servers!'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua'
+}
