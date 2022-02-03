db03607ab71a56523792e03603779e787cc7d362
xx-mandarijntje-xx <98543952+xx-mandarijntje-xx@users.noreply.github.com>
2022-02-03 03:47:58 +0100

    Update client.lua
    Added chatSuggestion

--- client.lua
+++ client.lua
@@ -2,3 +2,5 @@ RegisterCommand("discord", function()
      exports['mythic_notify']:DoHudText('inform', '📣 Discord: [YOUR OWN LINK]')
     
 end, false)
+
+TriggerEvent('chat:addSuggestion', '/discord', 'Join our Discord server with the link!!')
